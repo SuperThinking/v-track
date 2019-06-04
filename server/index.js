8d1e1ad96e20d44db76fedf2a3204faf4dc66a66
Vishal <superdhawan@gmail.com>
2019-06-04 12:42:42 +0530

    removed waste files, added option to fetch room number + minor additions

--- server/index.js
+++ server/index.js
@@ -13,103 +13,31 @@ app.use(
   })
 );
 
+app.get("/", (req, res, next) => {
+  res.send(
+    "<h2>Server for V-Track. <br/> Kindly contact <i>superthinkingdev@gmail.com (Vishal Dhawan)</i> for more info.</h2>"
+  );
+});
+
 app.post("/", (req, res, next) => {
   let id = req.body.id,
     pass = req.body.pass;
-  // res.send({
-  //   name: "VISHAL DHAWAN",
-  //   subjects: [
-  //     [
-  //       "CSE3021",
-  //       "Social and Information Networks",
-  //       "Embedded Theory",
-  //       "37",
-  //       "42",
-  //       "89",
-  //       "1229",
-  //       "ETH"
-  //     ],
-  //     [
-  //       "CSE3999",
-  //       "Technical  Answers  for Real World Problems (TARP)",
-  //       "Embedded Theory",
-  //       "13",
-  //       "14",
-  //       "93",
-  //       "2217",
-  //       "ETH"
-  //     ],
-  //     [
-  //       "CSE4003",
-  //       "Cyber Security",
-  //       "Embedded Theory",
-  //       "33",
-  //       "39",
-  //       "85",
-  //       "1246",
-  //       "ETH"
-  //     ],
-  //     [
-  //       "CSE4019",
-  //       "Image  Processing",
-  //       "Embedded Theory",
-  //       "39",
-  //       "40",
-  //       "98",
-  //       "1186",
-  //       "ETH"
-  //     ],
-  //     [
-  //       "CSE4022",
-  //       "Natural Language Processing",
-  //       "Embedded Theory",
-  //       "40",
-  //       "44",
-  //       "91",
-  //       "1220",
-  //       "ETH"
-  //     ],
-  //     [
-  //       "MGT1036",
-  //       "Principles of Marketing",
-  //       "Embedded Theory",
-  //       "38",
-  //       "42",
-  //       "91",
-  //       "2058",
-  //       "ETH"
-  //     ],
-  //     [
-  //       "STS3004",
-  //       "Data Structures and Algorithms",
-  //       "Soft Skill",
-  //       "41",
-  //       "43",
-  //       "96",
-  //       "1050",
-  //       "SS"
-  //     ]
-  //   ]
-  // });
   login.studentAuth(id, pass, (name, regno, cookieJ, err) => {
     if (!err) {
       let sem = "WS";
       let fromDate = "01-Jan-2015";
       let toDate = "01-Jan-2100";
-      if (new Date().getMonth() > 5 && new Date().getMonth() < 11) sem = "FS";
+      if (new Date().getMonth() >= 5 && new Date().getMonth() < 11) sem = "FS";
       const marksURL = `https://academicscc.vit.ac.in/student/marks1.asp?sem=${sem}`;
       const attendanceURL = `https://academicscc.vit.ac.in/student/attn_report.asp?sem=${sem}&fmdt=${fromDate}&todt=${toDate}`;
       const uattendanceURL =
         "https://academicscc.vit.ac.in/student/attn_report_details.asp";
-      // This Works//
-      attendance_module(cookieJ, marksURL, attendanceURL, res, name);
-      // uniqueAttendance(cookieJ, marksURL, uattendanceURL, res, []);
-      // res.send('ASDASD');
+      const ttURL = `https://academicscc.vit.ac.in/student/course_regular.asp?sem=${sem}`;
+      attendance_module(cookieJ, marksURL, attendanceURL, ttURL, res, name);
     } else {
       res.send({ Error: err });
     }
   });
-  // res.send('Hey');
 });
 
 app.listen(5000, () => {
--- server/modules/attendance_module.js
+++ server/modules/attendance_module.js
@@ -1,69 +1,196 @@
-var request = require('request')
-var cheerio = require('cheerio')
+var request = require("request");
+var cheerio = require("cheerio");
 
-getTotalAttendanceDetail = (cookieJ, marksURL, attendanceURL, res, name) => {
-    request.get(marksURL, { uri: marksURL, jar: cookieJ }, function (err, httpResponse, html) {
-        if (err) {
-            console.log(err)
+getTotalAttendanceDetail = (
+  cookieJ,
+  marksURL,
+  attendanceURL,
+  ttURL,
+  res,
+  name
+) => {
+  request.get(marksURL, { uri: marksURL, jar: cookieJ }, function(
+    err,
+    httpResponse,
+    html
+  ) {
+    if (err) {
+      console.log(err);
+    }
+    request.get(attendanceURL, { uri: attendanceURL, jar: cookieJ }, function(
+      err,
+      httpResponse,
+      html
+    ) {
+      if (err) {
+        console.log(err);
+      }
+      let $ = cheerio.load(html);
+      let k = $("tbody")
+        .last()
+        .children()
+        .first()
+        .next();
+      var AttObj = { name: name.trim(), subjects: [] };
+      var subObj = {};
+      while (k.text().trim()) {
+        let l = k.find("td").first();
+        let classnbr = k
+          .find("td")
+          .last()
+          .find("input")
+          .next()
+          .attr("value");
+        let crstp = k
+          .find("td")
+          .last()
+          .find("input")
+          .last()
+          .prev()
+          .attr("value");
+        let tempArr = [];
+        while (l.text().trim()) {
+          tempArr.push(l.text().trim());
+          l = l.next();
         }
-        request.get(attendanceURL, { uri: attendanceURL, jar: cookieJ }, function (err, httpResponse, html) {
-            if (err) {
-                console.log(err)
+        // console.log(tempArr[1], tempArr[2], tempArr[4], tempArr[6], tempArr[7], tempArr[8])
+        subObj[classnbr] = [
+          tempArr[1],
+          tempArr[2],
+          tempArr[3],
+          tempArr[6],
+          tempArr[7],
+          tempArr[8],
+          classnbr,
+          crstp,
+          tempArr[4]
+        ];
+        k = k.next();
+      }
+      request.get(ttURL, { uri: ttURL, jar: cookieJ }, function(
+        err,
+        httpResponse,
+        html
+      ) {
+        if (err) {
+          console.log(err);
+        }
+        request.get(ttURL, { uri: ttURL, jar: cookieJ }, function(
+          err,
+          httpResponse,
+          html
+        ) {
+          if (err) {
+            console.log(err);
+          }
+          let $ = cheerio.load(html);
+          $ = cheerio.load(
+            $("form")
+              .first()
+              .html()
+          );
+          var k = $("tbody")
+            .children()
+            .first()
+            .next();
+          while (k.text().trim()) {
+            let l = k.find("td").first();
+            let cn = l.text().trim();
+            if (!cn) {
+              l = l.next().next();
+              cn = l.text().trim();
             }
-            let $ = cheerio.load(html);
-            let k = $('tbody').last().children().first().next()
-            var AttObj = { 'name':name.trim(), 'subjects': [] }
-            while (k.text().trim()) {
-                let l = k.find('td').first()
-                let classnbr = k.find('td').last().find('input').next().attr('value')
-                let crstp = k.find('td').last().find('input').last().prev().attr('value')
-                let tempArr = []
-                while (l.text().trim()) {
-                    tempArr.push(l.text().trim())
-                    l = l.next()
-                }
-                // console.log(tempArr[1], tempArr[2], tempArr[4], tempArr[6], tempArr[7], tempArr[8])
-                AttObj.subjects.push([tempArr[1], tempArr[2], tempArr[3], tempArr[6], tempArr[7], tempArr[8], classnbr, crstp])
-                k = k.next()
+            if (cn in subObj) {
+              subObj[cn].push(
+                l
+                  .next()
+                  .next()
+                  .next()
+                  .next()
+                  .next()
+                  .next()
+                  .next()
+                  .next()
+                  .text()
+                  .trim()
+              );
             }
-            console.log(AttObj)
-            //Changed from html->AttObj
-            res.send(AttObj);
+            k = k.next();
+          }
+          for (i in subObj) {
+            AttObj.subjects.push(subObj[i]);
+          }
+          console.log(AttObj);
+          //Changed from html->AttObj
+          res.send(AttObj);
         });
+      });
     });
-}
+  });
+};
+
+getRoomNumber = (ttURL, cookieJ, h) => {};
 
 //vvvvvvvvvvv NOT WORKING vvvvvvvvvvv//
-getUniqueAttendanceDetail = (cookieJ, marksURL, attendanceURL, res, reqArray) => {
-    let sem = "WINSEM" + (new Date().getFullYear() - 1) + "-" + (new Date().getFullYear().toString().substr(2))
-    let fromDate = "01-Jan-2015"
-    let toDate = "01-Jan-2100"
-    if (new Date().getMonth() > 5 && new Date().getMonth() < 11)
-        sem = "FALLSEM" + (new Date().getFullYear()) + "-" + ((new Date().getFullYear() + 1).toString().substr(2))
-    let formData = {
-        'semcode': "WINSEM2018-19",
-        'classnbr': "1229",
-        'from_date': "01-Jan-2015",
-        'to_date': "01-Jan-2020",
-        'crscd': "CSE3021",
-        'crstp': "ETH"
+getUniqueAttendanceDetail = (
+  cookieJ,
+  marksURL,
+  attendanceURL,
+  res,
+  reqArray
+) => {
+  let sem =
+    "WINSEM" +
+    (new Date().getFullYear() - 1) +
+    "-" +
+    new Date()
+      .getFullYear()
+      .toString()
+      .substr(2);
+  let fromDate = "01-Jan-2015";
+  let toDate = "01-Jan-2100";
+  if (new Date().getMonth() > 5 && new Date().getMonth() < 11)
+    sem =
+      "FALLSEM" +
+      new Date().getFullYear() +
+      "-" +
+      (new Date().getFullYear() + 1).toString().substr(2);
+  let formData = {
+    semcode: "WINSEM2018-19",
+    classnbr: "1229",
+    from_date: "01-Jan-2015",
+    to_date: "01-Jan-2020",
+    crscd: "CSE3021",
+    crstp: "ETH"
+  };
+  console.log(formData);
+  request.get(marksURL, { uri: marksURL, jar: cookieJ }, function(
+    err,
+    httpResponse,
+    html
+  ) {
+    if (err) {
+      console.log(err);
     }
-    console.log(formData)
-    request.get(marksURL, { uri: marksURL, jar: cookieJ }, function (err, httpResponse, html) {
+    request.post(
+      attendanceURL,
+      {
+        jar: cookieJ,
+        formData: formData,
+        headers: { "Content-Type": "application/x-www-form-urlencoded" }
+      },
+      function(err, httpResponse, html) {
         if (err) {
-            console.log(err)
+          console.log(err);
         }
-        request.post(attendanceURL, { jar: cookieJ, formData:formData, headers:{"Content-Type": "application/x-www-form-urlencoded"} }, function (err, httpResponse, html) {
-            if (err) {
-                console.log(err)
-            }
-            console.log(html)
-            res.send(html)
-        });
-    });
-}
+        console.log(html);
+        res.send(html);
+      }
+    );
+  });
+};
 
 module.exports = {
-    getTotalAttendanceDetail: getTotalAttendanceDetail,
-    uniqueAttendance: getUniqueAttendanceDetail
-}
+  getTotalAttendanceDetail: getTotalAttendanceDetail,
+  uniqueAttendance: getUniqueAttendanceDetail
+};
