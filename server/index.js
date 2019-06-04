9dd5819a5334134a6c465e710fb012c52c7ed0c8
Vishal <superdhawan@gmail.com>
2019-06-04 19:25:39 +0530

    fix 101

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ var attendance_module = require("./modules/attendance_module")
 var uniqueAttendance = require("./modules/attendance_module").uniqueAttendance;
 var bodyParser = require("body-parser");
 
+const port = process.env.PORT || 5000;
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -40,6 +42,6 @@ app.post("/", (req, res, next) => {
   });
 });
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("Server is up on port " + 5000);
 });
